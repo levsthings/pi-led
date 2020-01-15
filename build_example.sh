536540e57a9669455ad11ef22e37c1615602c17d
Lev E <bearnode@gmail.com>
2020-01-15 17:28:09 +0300

    add error handling

---
+++ build_example.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+
+tag="$(git describe --tags $(git rev-list --tags --max-count=1))"
+filename="pi-temp-$tag-arm64-linux"
+
+env GOOS=linux GOARCH=arm GOARM=6 go build -o release/pi-display example/main.go &&
+    cd release && tar -czvf $filename.tar.gz pi-temp
